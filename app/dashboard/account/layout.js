75b415e06893f272f536e3dbad52377be3c0f2d2
Obiwumma Pascal <pobiwumma@gmail.com>
2025-11-25 14:49:04 +0100

    Design account layout UI

--- app/dashboard/account/layout.js
+++ app/dashboard/account/layout.js
@@ -1,13 +1,53 @@
-import React from 'react'
+import { Card } from "@/components/ui/card";
 
-function layout() {
+export default function AccountLayout({ children }) {
   return (
-    <div className='flex'>
-      {/* SIDENAV */}
-      <div></div>
-      <div></div>
-    </div>
-  )
-}
+      
+        <Card className="flex">
 
-export default layout
+          {/* SIDEBAR */}
+          <nav className=" text-gray-900 w-[80%] p-6">
+            
+            <div className=" flex flex-col justify-between ">
+              <div>
+                <ul className="grid gap-10">
+                  <li>
+                    <div>
+                      <h1>Account</h1>
+                      <p className="text-sm  text-gray-500">Manage your account details</p>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <h1>Password</h1>
+                      <p className="text-sm  text-gray-500">Change your password for security</p>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <h1>Notification</h1>
+                      <p className="text-sm  text-gray-500">Set your notification preferences</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <span>Log Out</span>
+              </div>
+            </div>
+          </nav>
+
+          {/* MAIN CONTENT */}
+          <div className=" w-full">
+
+            {/* PAGE CONTENT */}
+            <div className="p-6">
+              {children}
+            </div>
+          </div>
+
+        </Card>
+
+    
+  );
+}
