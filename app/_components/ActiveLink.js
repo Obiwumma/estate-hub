4134a8f3afd17105120f06936a5c763c05690346
Obiwumma Pascal <pobiwumma@gmail.com>
2025-12-21 17:35:33 +0100

    Create ActiveLink

---
+++ app/_components/ActiveLink.js
@@ -0,0 +1,18 @@
+"use client"; // This allows us to check the browser URL
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function ActiveLink({ href, children }) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <Link 
+      href={href} 
+      className={isActive ? "text-purple-600 font-bold" : "text-gray-900"}
+    >
+      {children}
+    </Link>
+  );
+}
